0d079b752b25cf4f2396a02c0203a8d13198b4eb
Spartak <spartaklysman@gmail.com>
2025-05-25 20:30:32 +0200

    JPA Repository, Controller, Service

---
+++ src/main/java/com/firstone/SoftwareEngineerController.java
@@ -0,0 +1,42 @@
+package com.firstone;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineersById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteSoftwareEngineer(@PathVariable Integer id) {
+        softwareEngineerService.deleteSoftwareEngineer(id);
+    }
+
+    @PutMapping("{id}")
+    public void updateSoftwareEngineer(@PathVariable Integer id,
+                                       @RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.updateSoftwareEngineer(id, softwareEngineer);
+    }
+}
---
+++ src/main/java/com/firstone/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.firstone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
